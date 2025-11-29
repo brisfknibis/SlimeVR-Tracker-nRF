5f1680a038108a909639f242f2de8bb47d29348e
sctanf <36978460+sctanf@users.noreply.github.com>
2025-11-29 02:27:38 -0600

    config remove excess comments and fix tabbing

--- src/config.c
+++ src/config.c
@@ -15,88 +15,88 @@
 struct config_settings_data *config_settings = (struct config_settings_data *)(DT_REG_ADDR(MEMORY_REGION) + RETAINED_SETTINGS_OFFSET);
 
 const uint16_t config_settings_count[] = {
-    CONFIG_0_SETTINGS_END,
-    CONFIG_1_SETTINGS_END,
-    CONFIG_2_SETTINGS_END,
-    CONFIG_3_SETTINGS_END
+	CONFIG_0_SETTINGS_END,
+	CONFIG_1_SETTINGS_END,
+	CONFIG_2_SETTINGS_END,
+	CONFIG_3_SETTINGS_END
 };
 
 const char *config_settings_names[] = {
-    "user_extra_actions", // 0
-    "ignore_reset",
-    "user_shutdown",
-    "use_imu_wakeup",
-    "delay_sleep_on_status",
-    "connection_over_hid",
-    "sensor_use_low_power_2", // 1
-    "use_imu_timeout",
-    "use_active_timeout",
-    "sensor_use_mag",
-    "use_sensor_clock",
-    "sensor_use_6_side_calibration",
-    "led_default_color_r", // 2
-    "led_default_color_g",
-    "led_default_color_b",
-    "active_timeout_mode",
-    "sensor_accel_odr",
-    "sensor_gyro_odr",
-    "sensor_accel_fs",
-    "sensor_gyro_fs",
-    "sensor_fusion",
-    "radio_tx_power",
-    "connection_timeout_delay", // 3
-    "sensor_lp_timeout",
-    "imu_timeout_ramp_min",
-    "imu_timeout_ramp_max",
-    "active_timeout_threshold",
-    "active_timeout_delay",
+	"user_extra_actions", // 0
+	"ignore_reset",
+	"user_shutdown",
+	"use_imu_wakeup",
+	"delay_sleep_on_status",
+	"connection_over_hid",
+	"sensor_use_low_power_2", // 1
+	"use_imu_timeout",
+	"use_active_timeout",
+	"sensor_use_mag",
+	"use_sensor_clock",
+	"sensor_use_6_side_calibration",
+	"led_default_color_r", // 2
+	"led_default_color_g",
+	"led_default_color_b",
+	"active_timeout_mode",
+	"sensor_accel_odr",
+	"sensor_gyro_odr",
+	"sensor_accel_fs",
+	"sensor_gyro_fs",
+	"sensor_fusion",
+	"radio_tx_power",
+	"connection_timeout_delay", // 3
+	"sensor_lp_timeout",
+	"imu_timeout_ramp_min",
+	"imu_timeout_ramp_max",
+	"active_timeout_threshold",
+	"active_timeout_delay",
 };
 
 const bool config_0_settings_defaults[16] = {
-    IS_ENABLED(CONFIG_USER_EXTRA_ACTIONS),
-    IS_ENABLED(CONFIG_IGNORE_RESET),
-    IS_ENABLED(CONFIG_USER_SHUTDOWN),
-    IS_ENABLED(CONFIG_USE_IMU_WAKEUP),
-    IS_ENABLED(CONFIG_DELAY_SLEEP_ON_STATUS),
-    IS_ENABLED(CONFIG_CONNECTION_OVER_HID),
+	IS_ENABLED(CONFIG_USER_EXTRA_ACTIONS),
+	IS_ENABLED(CONFIG_IGNORE_RESET),
+	IS_ENABLED(CONFIG_USER_SHUTDOWN),
+	IS_ENABLED(CONFIG_USE_IMU_WAKEUP),
+	IS_ENABLED(CONFIG_DELAY_SLEEP_ON_STATUS),
+	IS_ENABLED(CONFIG_CONNECTION_OVER_HID),
 };
 
 const bool config_1_settings_defaults[16] = {
-    IS_ENABLED(CONFIG_SENSOR_USE_LOW_POWER_2),
-    IS_ENABLED(CONFIG_USE_IMU_TIMEOUT),
-    IS_ENABLED(CONFIG_USE_ACTIVE_TIMEOUT),
-    IS_ENABLED(CONFIG_SENSOR_USE_MAG),
-    IS_ENABLED(CONFIG_USE_SENSOR_CLOCK),
-    IS_ENABLED(CONFIG_SENSOR_USE_6_SIDE_CALIBRATION),
+	IS_ENABLED(CONFIG_SENSOR_USE_LOW_POWER_2),
+	IS_ENABLED(CONFIG_USE_IMU_TIMEOUT),
+	IS_ENABLED(CONFIG_USE_ACTIVE_TIMEOUT),
+	IS_ENABLED(CONFIG_SENSOR_USE_MAG),
+	IS_ENABLED(CONFIG_USE_SENSOR_CLOCK),
+	IS_ENABLED(CONFIG_SENSOR_USE_6_SIDE_CALIBRATION),
 };
 
 const int16_t config_2_settings_defaults[16] = {
 #ifdef CONFIG_LED_DEFAULT_COLOR_R
-    CONFIG_LED_DEFAULT_COLOR_R, // 0-10000
+	CONFIG_LED_DEFAULT_COLOR_R, // 0-10000
 #else
 	4000,
 #endif
 #ifdef CONFIG_LED_DEFAULT_COLOR_G
-    CONFIG_LED_DEFAULT_COLOR_G, // 0-10000
+	CONFIG_LED_DEFAULT_COLOR_G, // 0-10000
 #else
 	6000,
 #endif
 #ifdef CONFIG_LED_DEFAULT_COLOR_B
-    CONFIG_LED_DEFAULT_COLOR_B, // 0-10000
+	CONFIG_LED_DEFAULT_COLOR_B, // 0-10000
 #else
 	0,
 #endif
 #if CONFIG_SHUTDOWN_ON_ACTIVE_TIMEOUT
-    1,
+	1,
 #else // CONFIG_SLEEP_ON_ACTIVE_TIMEOUT
 	0,
 #endif
-    CONFIG_SENSOR_ACCEL_ODR, // 0-3200 (ex.)
-    CONFIG_SENSOR_GYRO_ODR, // 0-3200 (ex.)
-    CONFIG_SENSOR_ACCEL_FS, // 0-32000 (ex.)
-    CONFIG_SENSOR_GYRO_FS, // 0-32000 (ex.)
+	CONFIG_SENSOR_ACCEL_ODR, // 0-3200 (ex.)
+	CONFIG_SENSOR_GYRO_ODR, // 0-3200 (ex.)
+	CONFIG_SENSOR_ACCEL_FS, // 0-32000 (ex.)
+	CONFIG_SENSOR_GYRO_FS, // 0-32000 (ex.)
 #if CONFIG_SENSOR_USE_VQF
-    3,
+	3,
 #elif CONFIG_SENSOR_USE_NXPSENSORFUSION
 	2,
 #elif CONFIG_SENSOR_USE_XIOFUSION
@@ -104,33 +104,33 @@ const int16_t config_2_settings_defaults[16] = {
 #else
 	0,
 #endif
-    CONFIG_RADIO_TX_POWER, // -128-127
+	CONFIG_RADIO_TX_POWER, // -128-127
 };
 
 const int32_t config_3_settings_defaults[16] = {
 #ifdef CONFIG_CONNECTION_TIMEOUT_DELAY
-    CONFIG_CONNECTION_TIMEOUT_DELAY,
+	CONFIG_CONNECTION_TIMEOUT_DELAY,
 #else
 	300000,
 #endif
-    CONFIG_SENSOR_LP_TIMEOUT,
+	CONFIG_SENSOR_LP_TIMEOUT,
 #ifdef CONFIG_IMU_TIMEOUT_RAMP_MIN
-    CONFIG_IMU_TIMEOUT_RAMP_MIN,
+	CONFIG_IMU_TIMEOUT_RAMP_MIN,
 #else
 	5000,
 #endif
 #ifdef CONFIG_IMU_TIMEOUT_RAMP_MAX
-    CONFIG_IMU_TIMEOUT_RAMP_MAX,
+	CONFIG_IMU_TIMEOUT_RAMP_MAX,
 #else
 	15000,
 #endif
 #ifdef CONFIG_ACTIVE_TIMEOUT_THRESHOLD
-    CONFIG_ACTIVE_TIMEOUT_THRESHOLD,
+	CONFIG_ACTIVE_TIMEOUT_THRESHOLD,
 #else
 	15000,
 #endif
 #ifdef CONFIG_ACTIVE_TIMEOUT_DELAY
-    CONFIG_ACTIVE_TIMEOUT_DELAY,
+	CONFIG_ACTIVE_TIMEOUT_DELAY,
 #else
 	900000,
 #endif
@@ -141,121 +141,121 @@ LOG_MODULE_REGISTER(config, LOG_LEVEL_INF);
 /*
 bool config_0_settings_read(uint16_t id)
 {
-    uint16_t read_mask = 1 << id;
+	uint16_t read_mask = 1 << id;
 
-    if (config_settings->config_0_ovrd & read_mask)
-        return config_settings->config_0_settings & read_mask;
+	if (config_settings->config_0_ovrd & read_mask)
+		return config_settings->config_0_settings & read_mask;
 
-    // Otherwise use default config value
-    return config_0_settings_defaults[id];
+	// Otherwise use default config value
+	return config_0_settings_defaults[id];
 }
 
 bool config_1_settings_read(uint16_t id)
 {
-    uint16_t read_mask = 1 << id;
+	uint16_t read_mask = 1 << id;
 
-    if (config_settings->config_1_ovrd & read_mask)
-        return config_settings->config_1_settings & read_mask;
+	if (config_settings->config_1_ovrd & read_mask)
+		return config_settings->config_1_settings & read_mask;
 
-    // Use default config value
-    return config_1_settings_defaults[id];
+	// Use default config value
+	return config_1_settings_defaults[id];
 }
 
 int16_t config_2_settings_read(uint16_t id)
 {
-    uint16_t read_mask = 1 << id;
+	uint16_t read_mask = 1 << id;
 
-    if (config_settings->config_2_ovrd & read_mask)
-        return config_settings->config_2_settings[id];
+	if (config_settings->config_2_ovrd & read_mask)
+		return config_settings->config_2_settings[id];
 
-    // Use default config value
-    return config_2_settings_defaults[id];
+	// Use default config value
+	return config_2_settings_defaults[id];
 }
 
 int32_t config_3_settings_read(uint16_t id)
 {
-    uint16_t read_mask = 1 << id;
+	uint16_t read_mask = 1 << id;
 
-    if (config_settings->config_3_ovrd & read_mask)
-        return config_settings->config_3_settings[id];
+	if (config_settings->config_3_ovrd & read_mask)
+		return config_settings->config_3_settings[id];
 
-    // Use default config value
-    return config_3_settings_defaults[id];
+	// Use default config value
+	return config_3_settings_defaults[id];
 }
 */
 
 void config_0_settings_write(uint16_t id, bool value)
 {
-    uint16_t write_mask = 1 << id;
+	uint16_t write_mask = 1 << id;
 
-    config_settings->config_0_ovrd |= write_mask;
+	config_settings->config_0_ovrd |= write_mask;
 
-    if (value)
-        config_settings->config_0_settings |= write_mask;
-    else
-        config_settings->config_0_settings &= ~write_mask;
+	if (value)
+		config_settings->config_0_settings |= write_mask;
+	else
+		config_settings->config_0_settings &= ~write_mask;
 
-    sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
+	sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
 }
 
 void config_1_settings_write(uint16_t id, bool value)
 {
-    uint16_t write_mask = 1 << id;
+	uint16_t write_mask = 1 << id;
 
-    config_settings->config_1_ovrd |= write_mask;
+	config_settings->config_1_ovrd |= write_mask;
 
-    if (value)
-        config_settings->config_1_settings |= write_mask;
-    else
-        config_settings->config_1_settings &= ~write_mask;
+	if (value)
+		config_settings->config_1_settings |= write_mask;
+	else
+		config_settings->config_1_settings &= ~write_mask;
 
-    sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
+	sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
 }
 
 void config_2_settings_write(uint16_t id, int16_t value)
 {
-    uint16_t write_mask = 1 << id;
+	uint16_t write_mask = 1 << id;
 
-    config_settings->config_2_ovrd |= write_mask;
+	config_settings->config_2_ovrd |= write_mask;
 
-    config_settings->config_2_settings[id] = value;
+	config_settings->config_2_settings[id] = value;
 
-    sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
+	sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
 }
 
 void config_3_settings_write(uint16_t id, int32_t value)
 {
-    uint16_t write_mask = 1 << id;
+	uint16_t write_mask = 1 << id;
 
-    config_settings->config_3_ovrd |= write_mask;
+	config_settings->config_3_ovrd |= write_mask;
 
-    config_settings->config_3_settings[id] = value;
+	config_settings->config_3_settings[id] = value;
 
-    sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
+	sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
 }
 
 void config_settings_reset(uint16_t config, uint16_t id)
 {
-    uint16_t write_mask = 1 << id;
+	uint16_t write_mask = 1 << id;
 
-    switch (config)
-    {
-    case 0:
-        config_settings->config_0_ovrd &= ~write_mask;
-        break;
-    case 1:
-        config_settings->config_1_ovrd &= ~write_mask;
-        break;
-    case 2:
-        config_settings->config_2_ovrd &= ~write_mask;
-        break;
-    case 3:
-        config_settings->config_3_ovrd &= ~write_mask;
-        break;
-    default:
-    }
+	switch (config)
+	{
+	case 0:
+		config_settings->config_0_ovrd &= ~write_mask;
+		break;
+	case 1:
+		config_settings->config_1_ovrd &= ~write_mask;
+		break;
+	case 2:
+		config_settings->config_2_ovrd &= ~write_mask;
+		break;
+	case 3:
+		config_settings->config_3_ovrd &= ~write_mask;
+		break;
+	default:
+	}
 
-    sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
+	sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
 }
 
 void config_settings_reset_all(void)
@@ -269,12 +269,12 @@ void config_settings_reset_all(void)
 	}
 	printk("Resetting device settings\n");
 
-    config_settings->config_0_ovrd = 0;
-    config_settings->config_1_ovrd = 0;
-    config_settings->config_2_ovrd = 0;
-    config_settings->config_3_ovrd = 0;
+	config_settings->config_0_ovrd = 0;
+	config_settings->config_1_ovrd = 0;
+	config_settings->config_2_ovrd = 0;
+	config_settings->config_3_ovrd = 0;
 
-    sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
+	sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
 	reset_confirm = false;
-    LOG_INF("Device settings reset");
+	LOG_INF("Device settings reset");
 }
--- src/config.h
+++ src/config.h
@@ -3,39 +3,18 @@
 
 #include "retained.h"
 
-// wrap nvs/retain read/write
-// nvs SETTINGS_ID
-// retain retained->settings
-// config_settings_id is the word offset
-
-// ops: read/write/reset
-
-// config 0 and 1 are boolean flags
-// config 2 is word array
-
-// read bool (config 0): id, read OVRD_0 from retain, read CONFIG_0_DEVICE_CONFIG from retain if needed, return
-// read bool (config 1): id
-// read int16 (config 2): id
-// read int32 (config 3): id
-
-// w
-//memcpy(retained->settings + addr, &val, 2);
-//sys_write(SETTINGS_ID, NULL, retained->settings, sizeof(retained->settings));
-// r
-//memcpy(&val, retained->settings + addr * 2, 2);
-
 #define CONFIG_SETTINGS_COUNT 4
 
 // map of settings area
 struct config_settings_data {
-    uint16_t config_0_ovrd; // Override enable flags
-    uint16_t config_0_settings;
-    uint16_t config_1_ovrd;
-    uint16_t config_1_settings;
-    uint16_t config_2_ovrd;
-    int16_t config_2_settings[16];
-    uint16_t config_3_ovrd;
-    int32_t config_3_settings[16];
+	uint16_t config_0_ovrd; // Override enable flags
+	uint16_t config_0_settings;
+	uint16_t config_1_ovrd;
+	uint16_t config_1_settings;
+	uint16_t config_2_ovrd;
+	int16_t config_2_settings[16];
+	uint16_t config_3_ovrd;
+	int32_t config_3_settings[16];
 };
 
 // because settings read is a macro, need to expose settings struct and defaults
@@ -43,48 +22,48 @@ extern struct config_settings_data *config_settings;
 
 // device settings
 enum config_0_settings_id {
-    CONFIG_0_USER_EXTRA_ACTIONS,
-    CONFIG_0_IGNORE_RESET,
-    CONFIG_0_USER_SHUTDOWN,
-    CONFIG_0_USE_IMU_WAKEUP,
-    CONFIG_0_DELAY_SLEEP_ON_STATUS,
-    CONFIG_0_CONNECTION_OVER_HID,
-    CONFIG_0_SETTINGS_END
+	CONFIG_0_USER_EXTRA_ACTIONS,
+	CONFIG_0_IGNORE_RESET,
+	CONFIG_0_USER_SHUTDOWN,
+	CONFIG_0_USE_IMU_WAKEUP,
+	CONFIG_0_DELAY_SLEEP_ON_STATUS,
+	CONFIG_0_CONNECTION_OVER_HID,
+	CONFIG_0_SETTINGS_END
 };
 
 // sensor settings
 enum config_1_settings_id {
-    CONFIG_1_SENSOR_USE_LOW_POWER_2,
-    CONFIG_1_USE_IMU_TIMEOUT,
-    CONFIG_1_USE_ACTIVE_TIMEOUT,
-    CONFIG_1_SENSOR_USE_MAG,
-    CONFIG_1_USE_SENSOR_CLOCK,
-    CONFIG_1_SENSOR_USE_6_SIDE_CALIBRATION,
-    CONFIG_1_SETTINGS_END
+	CONFIG_1_SENSOR_USE_LOW_POWER_2,
+	CONFIG_1_USE_IMU_TIMEOUT,
+	CONFIG_1_USE_ACTIVE_TIMEOUT,
+	CONFIG_1_SENSOR_USE_MAG,
+	CONFIG_1_USE_SENSOR_CLOCK,
+	CONFIG_1_SENSOR_USE_6_SIDE_CALIBRATION,
+	CONFIG_1_SETTINGS_END
 };
 
 enum config_2_settings_id {
-    CONFIG_2_LED_DEFAULT_COLOR_R,
-    CONFIG_2_LED_DEFAULT_COLOR_G,
-    CONFIG_2_LED_DEFAULT_COLOR_B,
-    CONFIG_2_ACTIVE_TIMEOUT_MODE, // 0: Sleep, 1: Shutdown
-    CONFIG_2_SENSOR_ACCEL_ODR,
-    CONFIG_2_SENSOR_GYRO_ODR,
-    CONFIG_2_SENSOR_ACCEL_FS,
-    CONFIG_2_SENSOR_GYRO_FS,
-    CONFIG_2_SENSOR_FUSION, // 1: XIO, 3: VQF
-    CONFIG_2_RADIO_TX_POWER,
-    CONFIG_2_SETTINGS_END
+	CONFIG_2_LED_DEFAULT_COLOR_R,
+	CONFIG_2_LED_DEFAULT_COLOR_G,
+	CONFIG_2_LED_DEFAULT_COLOR_B,
+	CONFIG_2_ACTIVE_TIMEOUT_MODE, // 0: Sleep, 1: Shutdown
+	CONFIG_2_SENSOR_ACCEL_ODR,
+	CONFIG_2_SENSOR_GYRO_ODR,
+	CONFIG_2_SENSOR_ACCEL_FS,
+	CONFIG_2_SENSOR_GYRO_FS,
+	CONFIG_2_SENSOR_FUSION, // 1: XIO, 3: VQF
+	CONFIG_2_RADIO_TX_POWER,
+	CONFIG_2_SETTINGS_END
 };
 
 enum config_3_settings_id {
-    CONFIG_3_CONNECTION_TIMEOUT_DELAY,
-    CONFIG_3_SENSOR_LP_TIMEOUT,
-    CONFIG_3_IMU_TIMEOUT_RAMP_MIN,
-    CONFIG_3_IMU_TIMEOUT_RAMP_MAX,
-    CONFIG_3_ACTIVE_TIMEOUT_THRESHOLD,
-    CONFIG_3_ACTIVE_TIMEOUT_DELAY,
-    CONFIG_3_SETTINGS_END
+	CONFIG_3_CONNECTION_TIMEOUT_DELAY,
+	CONFIG_3_SENSOR_LP_TIMEOUT,
+	CONFIG_3_IMU_TIMEOUT_RAMP_MIN,
+	CONFIG_3_IMU_TIMEOUT_RAMP_MAX,
+	CONFIG_3_ACTIVE_TIMEOUT_THRESHOLD,
+	CONFIG_3_ACTIVE_TIMEOUT_DELAY,
+	CONFIG_3_SETTINGS_END
 };
 
 extern const uint16_t config_settings_count[];
@@ -116,379 +95,3 @@ void config_settings_reset(uint16_t config, uint16_t id);
 void config_settings_reset_all(void);
 
 #endif
-
-/*
-menu "Status LED default status color"
-    depends on LED_RGB_COLOR
-
-config LED_DEFAULT_COLOR_R
-    int "Red value"
-    range 0 10000
-    default 4000
-    depends on LED_RGB_COLOR
-
-config LED_DEFAULT_COLOR_G
-    int "Green value"
-    range 0 10000
-    default 6000
-    depends on LED_RGB_COLOR
-
-config LED_DEFAULT_COLOR_B
-    int "Blue value"
-    range 0 10000
-    default 0
-    depends on LED_RGB_COLOR
-
-endmenu
-
-config USER_EXTRA_ACTIONS
-    bool "Multiple press actions"
-
-config IGNORE_RESET
-    bool "Ignore reset"
-    default y
-
-config USER_SHUTDOWN
-    bool "User shutdown support"
-    default y
-
-config USE_IMU_WAKE_UP
-    bool "IMU wake up support"
-    default y
-
-config DELAY_SLEEP_ON_STATUS
-    bool "Delay IMU wake up mode on status flags"
-    default y
-    depends on USE_IMU_WAKE_UP
-
-config WOM_USE_DCDC
-    bool "Use DCDC in IMU wake up mode"
-    depends on USE_IMU_WAKE_UP
-
-config CONNECTION_TIMEOUT_DELAY
-    int "Connection timeout delay (ms)"
-    default 300000
-    depends on USER_SHUTDOWN
-
-menu "Sensor power saving"
-
-config SENSOR_LP_TIMEOUT
-    int "Sensor low power delay (ms)"
-    default 500
-
-config SENSOR_USE_LOW_POWER_2
-    bool "Use additional low power modes"
-
-config USE_IMU_TIMEOUT
-    bool "Use IMU wake up state"
-    default y
-    depends on USE_IMU_WAKE_UP
-
-config IMU_TIMEOUT_RAMP_MIN
-    int "Sensor timeout minimum delay (ms)"
-    default 5000
-    depends on USE_IMU_TIMEOUT || SENSOR_USE_LOW_POWER_2
-
-config IMU_TIMEOUT_RAMP_MAX
-    int "Sensor timeout maximum delay (ms)"
-    default 15000
-    depends on USE_IMU_TIMEOUT || SENSOR_USE_LOW_POWER_2
-
-config USE_ACTIVE_TIMEOUT
-    bool "Use activity timeout"
-    default y
-    depends on USE_IMU_WAKE_UP || USER_SHUTDOWN
-
-choice
-store as single int instead!
-    prompt "Activity timeout mode"
-    default SLEEP_ON_ACTIVE_TIMEOUT
-    depends on USE_ACTIVE_TIMEOUT
-
-config SLEEP_ON_ACTIVE_TIMEOUT
-    bool "IMU wake up"
-    depends on USE_IMU_WAKE_UP
-
-config SHUTDOWN_ON_ACTIVE_TIMEOUT
-    bool "User shutdown"
-    depends on USER_SHUTDOWN
-
-endchoice
-
-config ACTIVE_TIMEOUT_THRESHOLD
-    int "Activity timeout threshold (ms)"
-    default 15000
-    depends on USE_ACTIVE_TIMEOUT
-
-config ACTIVE_TIMEOUT_DELAY
-    int "Activity timeout delay (ms)"
-    default 900000
-    depends on USE_ACTIVE_TIMEOUT
-
-endmenu
-
-config SENSOR_ACCEL_ODR
-    int "Accelerometer output data rate (Hz)"
-    default 100
-
-config SENSOR_GYRO_ODR
-    int "Gyrometer output data rate (Hz)"
-    default 200
-
-config SENSOR_ACCEL_FS
-    int "Accelerometer full scale (g)"
-    default 4
-
-config SENSOR_GYRO_FS
-    int "Gyrometer full scale (dps)"
-    default 1000
-
-config SENSOR_USE_MAG
-    bool "Magnetometer support"
-    default y
-
-config USE_SENSOR_CLOCK
-    bool "Use external IMU clock"
-    default y
-
-choice
-store as single int instead!
-	prompt "Sensor fusion"
-    default SENSOR_USE_VQF
-
-config SENSOR_USE_XIOFUSION
-    bool "Use x-io Technologies Fusion"
-
-config SENSOR_USE_VQF
-    bool "Use VQF"
-
-endchoice
-
-config SENSOR_USE_6_SIDE_CALIBRATION
-    bool "Use 6-side calibration"
-    default y
-    depends on USE_SLIMENRF_CONSOLE
-
-config RADIO_TX_POWER
-    int "Radio output power (dBm)"
-    default 8
-
-config CONNECTION_OVER_HID
-    bool "Use HID for data output"
-    default n
-
-*/
-
-/*
-schema:
-storage is all in a 16bit array for ints
-second binary array for bools
-
-can address the config system by writing/reading words
-for bools, similar but use 0/1? or true/false?
-
-this has to be stored efficiently in retain, probably fixed array sizes
-for nvs, the size can be variable depending on how many settings are known to the device
-*/
-
-
-/*
-menu "Status LED default status color"
-    depends on LED_RGB_COLOR
-
-config LED_DEFAULT_COLOR_R
-    int "Red value"
-    range 0 10000
-    default 4000
-    depends on LED_RGB_COLOR
-
-config LED_DEFAULT_COLOR_G
-    int "Green value"
-    range 0 10000
-    default 6000
-    depends on LED_RGB_COLOR
-
-config LED_DEFAULT_COLOR_B
-    int "Blue value"
-    range 0 10000
-    default 0
-    depends on LED_RGB_COLOR
-
-endmenu
-
-config USER_EXTRA_ACTIONS
-    bool "Multiple press actions"
-
-config IGNORE_RESET
-    bool "Ignore reset"
-    default y
-
-config USER_SHUTDOWN
-    bool "User shutdown support"
-    default y
-
-config USE_IMU_WAKE_UP
-    bool "IMU wake up support"
-    default y
-
-config DELAY_SLEEP_ON_STATUS
-    bool "Delay IMU wake up mode on status flags"
-    default y
-    depends on USE_IMU_WAKE_UP
-
-config WOM_USE_DCDC
-    bool "Use DCDC in IMU wake up mode"
-    depends on USE_IMU_WAKE_UP
-
-config CONNECTION_TIMEOUT_DELAY
-    int "Connection timeout delay (ms)"
-    default 300000
-    depends on USER_SHUTDOWN
-
-menu "Sensor power saving"
-
-config SENSOR_LP_TIMEOUT
-    int "Sensor low power delay (ms)"
-    default 500
-
-config SENSOR_USE_LOW_POWER_2
-    bool "Use additional low power modes"
-
-config USE_IMU_TIMEOUT
-    bool "Use IMU wake up state"
-    default y
-    depends on USE_IMU_WAKE_UP
-
-config IMU_TIMEOUT_RAMP_MIN
-    int "Sensor timeout minimum delay (ms)"
-    default 5000
-    depends on USE_IMU_TIMEOUT || SENSOR_USE_LOW_POWER_2
-
-config IMU_TIMEOUT_RAMP_MAX
-    int "Sensor timeout maximum delay (ms)"
-    default 15000
-    depends on USE_IMU_TIMEOUT || SENSOR_USE_LOW_POWER_2
-
-config USE_ACTIVE_TIMEOUT
-    bool "Use activity timeout"
-    default y
-    depends on USE_IMU_WAKE_UP || USER_SHUTDOWN
-
-choice
-store as single int instead!
-    prompt "Activity timeout mode"
-    default SLEEP_ON_ACTIVE_TIMEOUT
-    depends on USE_ACTIVE_TIMEOUT
-
-config SLEEP_ON_ACTIVE_TIMEOUT
-    bool "IMU wake up"
-    depends on USE_IMU_WAKE_UP
-
-config SHUTDOWN_ON_ACTIVE_TIMEOUT
-    bool "User shutdown"
-    depends on USER_SHUTDOWN
-
-endchoice
-
-config ACTIVE_TIMEOUT_THRESHOLD
-    int "Activity timeout threshold (ms)"
-    default 15000
-    depends on USE_ACTIVE_TIMEOUT
-
-config ACTIVE_TIMEOUT_DELAY
-    int "Activity timeout delay (ms)"
-    default 900000
-    depends on USE_ACTIVE_TIMEOUT
-
-endmenu
-
-config SENSOR_ACCEL_ODR
-    int "Accelerometer output data rate (Hz)"
-    default 100
-
-config SENSOR_GYRO_ODR
-    int "Gyrometer output data rate (Hz)"
-    default 200
-
-config SENSOR_ACCEL_FS
-    int "Accelerometer full scale (g)"
-    default 4
-
-config SENSOR_GYRO_FS
-    int "Gyrometer full scale (dps)"
-    default 1000
-
-config SENSOR_USE_MAG
-    bool "Magnetometer support"
-    default y
-
-config USE_SENSOR_CLOCK
-    bool "Use external IMU clock"
-    default y
-
-choice
-store as single int instead!
-	prompt "Sensor fusion"
-    default SENSOR_USE_VQF
-
-config SENSOR_USE_XIOFUSION
-    bool "Use x-io Technologies Fusion"
-
-config SENSOR_USE_VQF
-    bool "Use VQF"
-
-endchoice
-
-config SENSOR_USE_6_SIDE_CALIBRATION
-    bool "Use 6-side calibration"
-    default y
-    depends on USE_SLIMENRF_CONSOLE
-
-config RADIO_TX_POWER
-    int "Radio output power (dBm)"
-    default 8
-
-config CONNECTION_OVER_HID
-    bool "Use HID for data output"
-    default n
-
-*/
-
-/*
-write/read to settings area performed as a write/read of arbitrary bytes (lol)
-write byte <hex> <int>
-write word <hex> <int>
-write all <base64>
-read byte <hex> -> <int>
-read word <hex> -> <int>
-read all -> <base64>
-*/
-
-/*
-i    led default color r/g/b
-b    user extra actions
-b    ignore reset
-b    user shutdown
-b    use imu wakeup
-b    delay sleep on status
-i    connection timeout delay
-i    lp timeout
-b    use lp2
-b    use imu timeout
-i    imu timeout ramp min
-i    imu timeout ramp max
-b    use active timeout
-i    sleep/shutdown on active timeout
-i    active timeout threshold
-i    active timeout delay
-i    sensor accel odr
-i    sensor gyro odr
-i    sensor accel fs
-i    sensor gyro fs
-b    use mag
-b    ?use sensor clock
-i    xiofusion/vqf
-b    6 side
-i    radio tx power
-b    connection over hid
-*/
